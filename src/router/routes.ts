ed3c50354562fb2d5eb27d1f1bf8f8bb6a6371a2
Gergo Hollo <hollogergo92@gmail.com>
2022-12-10 22:16:45 +0100

    delete unnecessary files, add home view

---
+++ src/router/routes.ts
@@ -0,0 +1,12 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+
+  ],
+  linkActiveClass: "nav-item is-active",
+  strict: true
+})
+
+export default router
