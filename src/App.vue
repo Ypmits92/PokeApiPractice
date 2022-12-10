ec220b21f7a67a22e562f188dd00f52bbe1dfae1
Gergo Hollo <hollogergo92@gmail.com>
2022-12-10 22:43:08 +0100

    create navbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import Navigation from "@/components/Navigation.vue";
 
 </script>
 
 <template>
-
+  <Navigation></Navigation>
+  <router-view></router-view>
 </template>
 
 <style scoped>
