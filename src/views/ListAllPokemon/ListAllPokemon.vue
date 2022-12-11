a66667fa3f04a9d5498bcc54e7a0776ac061e45e
Gergo Hollo <hollogergo92@gmail.com>
2022-12-11 15:58:30 +0100

    make pokemon name to link, create basic pokemon detail component, and added to routes

--- src/views/ListAllPokemon/ListAllPokemon.vue
+++ src/views/ListAllPokemon/ListAllPokemon.vue
@@ -1,4 +1,6 @@
 <script>
+import pokemonDetail from "@/views/PokemonDetail/PokemonDetail.vue";
+
 export default {
   data() {
     return {
@@ -11,6 +13,38 @@ export default {
     const response = await fetch("https://pokeapi.co/api/v2/pokemon?offset=" + this.offset + "&limit=" + this.limit);
     const result = await response.json();
     this.pokemons = result.results;
+  },
+  methods: {
+    pokemonDetail() {
+      return pokemonDetail
+    },
+    async displayPrevious() {
+      if (this.offset >= 20) {
+        this.offset -= this.limit;
+      } else {
+        this.offset = 0;
+      }
+
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon?offset=" + this.offset + "&limit=" + this.limit);
+      const result = await response.json();
+      this.pokemons.slice();
+      this.pokemons = result.results;
+    },
+    async displayNext() {
+      if (this.offset <= 111) {
+        this.offset += this.limit;
+      } else {
+        this.offset = 131;
+      }
+
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon?offset=" + this.offset + "&limit=" + this.limit);
+      const result = await response.json();
+      this.pokemons.slice();
+      this.pokemons = result.results;
+    },
+    createUrl(pokemon) {
+      return "all/" + pokemon.name;
+    }
   }
 }
 </script>
@@ -18,8 +52,10 @@ export default {
 <template>
   <h1 class="title is-2">List of all Pokemon</h1>
   <ul>
-    <li v-for="pokemon in pokemons">{{pokemon.name}}</li>
+    <li v-for="pokemon in pokemons"><router-link :to=this.createUrl(pokemon) >{{pokemon.name}}</router-link></li>
   </ul>
+  <button class="button is-danger" @click="displayPrevious">Previous</button>
+  <button class="button is-success" @click="displayNext">Next</button>
 </template>
 
 <style scoped>
---
+++ src/views/PokemonDetail/PokemonDetail.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "PokemonDetail"
+}
+</script>
+
+<template>
+<h1>AAAAAAAAAAAA</h1>
+</template>
+
+<style scoped>
+
+</style>
